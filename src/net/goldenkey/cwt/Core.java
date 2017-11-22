08b66ed2e7aef321b0b2018b8229ba00e78d51a4
Antonio <antonio57gines57@gmail.com>
2017-11-22 20:57:12 +0100

    BaseJuego

---
+++ src/net/goldenkey/cwt/Core.java
@@ -0,0 +1,19 @@
+package net.goldenkey.cwt;
+
+import java.applet.Applet;
+
+import javax.swing.JFrame;
+
+public class Core extends Applet implements Runnable{
+
+	private static final long serialVersionUID= 1L;
+	private JFrame frame;
+	public static final int resolution =1;
+	
+	//https://youtu.be/MrsNKi30WsQ?t=574
+	
+	public void run() {
+		
+	}
+
+}

dd33e2848b2daa13e0f83fc1a391a7d5d00fb66e
ASPIRE-TC\Dimitri <dvmdvm97@gmail.com>
2017-11-23 23:27:04 +0100

    Agregadas las librerias

--- src/simpleslickgame/Main.java
+++ src/simpleslickgame/Main.java
@@ -1,9 +1,12 @@
 package simpleslickgame;
-import org.newdawn.slick.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
  
@@ -17,21 +20,36 @@ public class Main extends BasicGame {
     @Override
     public void init(GameContainer gc) throws SlickException {
         this.rectangulo = new Rectangle(50, 50, 100, 100);
+        
     }
   
     @Override
     public void update(GameContainer gc, int i) throws SlickException {
-    	 Input input =gc.getInput();
-    	 if (input.isKeyDown(Input.KEY_RIGHT)){
-    		rectangulo.setX(rectangulo.getX()+1); 
-    	 }
+    	Input input= gc.getInput();
+    	if (input.isKeyDown(Input.KEY_DOWN)) {
+    		rectangulo.setY(rectangulo.getY()+1);
+    	}
+    	else if  (input.isKeyDown(Input.KEY_UP)){
+         rectangulo.setY(rectangulo.getY()-1); //Para arriba negativo y para arriba positivo
+    	}
+    	else if (input.isKeyDown(Input.KEY_RIGHT)) {
+    		rectangulo.setX(rectangulo.getX()+1);
+    
+    	}
+    	else if (input.isKeyDown(Input.KEY_LEFT)) {
+    		rectangulo.setX(rectangulo.getX()-1);
+    	}
+    	else if (input.isKeyPressed(Input.KEY_R)) {
+    		rectangulo.setX(0);
+    		rectangulo.setY(0);
+    		
+    	}
     }
-   
-    
-    
+  
     @Override
     public void render(GameContainer gc, Graphics g) throws SlickException {
         g.draw(this.rectangulo);
+        
     }
   
     public static void main(String[] args){
@@ -41,8 +59,7 @@ public class Main extends BasicGame {
             appgc.start();
         }
         catch (SlickException ex) {
-            System.out.println("Hay un error pardillo");
-        	//Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
